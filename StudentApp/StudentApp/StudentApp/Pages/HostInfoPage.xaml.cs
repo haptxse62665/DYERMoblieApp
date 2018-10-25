a8dace0c248dcf96224880a1977aaf35797be921
VuHN <vu23071997@gmail.com>
2018-10-25 15:03:31 +0700

    VuHN_Update StudentApp_ Call_ Mail and Map Location

--- StudentApp/StudentApp/StudentApp/Pages/HostInfoPage.xaml.cs
+++ StudentApp/StudentApp/StudentApp/Pages/HostInfoPage.xaml.cs
@@ -16,5 +16,19 @@ namespace StudentApp.Pages
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private void Call_Tapped(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("tel:+123 456 789"));
+        }
+        private void Mail_Tapped(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("mailto:fptUniversity@test.com?subject=Your issue &body=Your info and your issue's detail"));
+        }
+        private void Address_Tapped(object sender, EventArgs e)
+        {
+            //Device.OpenUri(new Uri("geo:" + _branch.Latitude + "," + _branch.Longitude));
+            Device.OpenUri(new Uri("http://maps.google.com/?daddr=4.912967,114.888674"));
+        }
+    }
 }
