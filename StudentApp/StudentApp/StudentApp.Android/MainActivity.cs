51dfddf588eefbe0ee615353a4a4f3677583e165
VuHN <vu23071997@gmail.com>
2018-10-26 02:39:36 +0700

    VuHN_Updated splash page and icon for Student App

--- StudentApp/StudentApp/StudentApp.Android/MainActivity.cs
+++ StudentApp/StudentApp/StudentApp.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using Plugin.CurrentActivity;
 
 namespace StudentApp.Droid
 {
-    [Activity(Label = "StudentApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "StudentApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
