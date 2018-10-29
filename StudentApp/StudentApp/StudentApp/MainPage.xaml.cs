bd20e173a522173f5f4ff4c2bfc01b1d8df6c6b1
VuHN <vu23071997@gmail.com>
2018-10-29 09:51:31 +0700

    VuHN_Update Mainpage.cs(login)

--- StudentApp/StudentApp/StudentApp/MainPage.xaml.cs
+++ StudentApp/StudentApp/StudentApp/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using StudentApp.Models;
 using StudentApp.Pages;
 using Xamarin.Forms;
 
@@ -12,7 +14,7 @@ namespace StudentApp
 {
     public partial class MainPage : MasterDetailPage
     {
-        string url = "";
+        string url = "202.190.1.102:8082/Token";
         public MainPage()
         {
             InitializeComponent();
@@ -21,6 +23,7 @@ namespace StudentApp
 
         private async void Button_Login(object sender, EventArgs e)
         {
+
             //get username and password
             var dict = new Dictionary<string, string>();
             dict.Add("username", Username.Text);
@@ -28,42 +31,47 @@ namespace StudentApp
             dict.Add("grant_type", "password");
             var client = new HttpClient();
 
-            //try
-            //{
-            //    var reponse = await client.PostAsync(url, new FormUrlEncodedContent(dict));
-            //    if (reponse.IsSuccessStatusCode)
-            //    {
-            //        var text = reponse.Content.ReadAsStringAsync();
+            try
+            {
+                var response = await client.PostAsync(url, new FormUrlEncodedContent(dict));
+                if (response.IsSuccessStatusCode)
+                {
+                    //var text = response.Content.ReadAsStringAsync(); //Json response from API
 
-            //        var token = JsonConvert.DeserializeObject<JsonToken>(text.Result);
-            //    }
-            //}
-            //catch (Exception)
-            //{
+                    //var token = JsonConvert.DeserializeObject<JsonToken>(text.Result);
 
-            //    throw;
-            //}
 
-            //if (Username.Text == "vu@fpt.edu.vn" && Password.Text == "23071997") {
-            //    Detail = new NavigationPage(new NotificationOverview())
+                    var content = await response.Content.ReadAsStringAsync();
+                    JObject jwtDynamic = JsonConvert.DeserializeObject<dynamic>(content);
+                    var accessToken = jwtDynamic.Value<string>("access_token");
+                    var tokenTypes = jwtDynamic.Value<string>("token_types");
+                    Helpers.Settings.TokenSettings = tokenTypes + " " + accessToken;
+                    Detail = new NavigationPage(new HostInfoPage())
+                    {
+                        BarBackgroundColor = Color.FromHex("#254F6E"),
+                        BarTextColor = Color.White
+                    };
+
+                    IsPresented = false;
+                    IsGestureEnabled = true;
+                }
+            }
+            catch (Exception)
+            {
+                DisplayAlert("Login fail", "Your username or password is incorrect", "OK");
+            }
+
+
+
+            //direct login
+            //Detail = new NavigationPage(new NotificationOverview())
             //{
             //    BarBackgroundColor = Color.FromHex("#254F6E"),
             //    BarTextColor = Color.White
             //};
 
-            //    IsPresented = false;
-            //    IsGestureEnabled = true;
-            //} 
-            //else DisplayAlert("Login fail", "Your username or password is incorrect", "OK");
-
-            Detail = new NavigationPage(new NotificationOverview())
-            {
-                BarBackgroundColor = Color.FromHex("#254F6E"),
-                BarTextColor = Color.White
-            };
-
-            IsPresented = false;
-            IsGestureEnabled = true;
+            //IsPresented = false;
+            //IsGestureEnabled = true;
         }
         private void Button_HostInfo(object sender, EventArgs e)
         {
